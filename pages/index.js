830a9e8fda8dd649ebf6b2334414ee4725089cf7
Ally McGilloway <allymcgilloway@Allys-MacBook-Pro.local>
2023-08-30 13:14:51 +0100

    Adds reset

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,11 @@ export default function Home() {
     7: "",
     8: ""
   });
+
   const [won, setWon] = useState(false);
   const [draw, setDraw] = useState(false);
+  const [winningLine, setWinningLine] = useState([]);
+  const [modalTitle, setModalTitle] = useState("");
 
   const winningCombo = [
     [0, 1, 2],
@@ -29,26 +32,29 @@ export default function Home() {
   ]
 
   useEffect(() => {
-    checkDraw();
-    checkWinner();
+    const isWon = checkWinner();
+    checkDraw(isWon);
+
   }, [boardData])
 
   const updateBoardData = (idx) => {
-    if (!boardData[idx]){
-      let value = xTurn === true ?  "X" : "O";
-      setBoardData({...boardData, [idx]: value});
+    if (!boardData[idx]) {
+      let value = xTurn === true ? "X" : "O";
+      setBoardData({ ...boardData, [idx]: value });
       setXTurn(!xTurn);
     }
   };
 
-  const checkDraw = () => {
+  const checkDraw = (isWon) => {
     let check = Object.keys(boardData).every((v) => boardData[v]);
-    console.log(won);
-    if (check && !won) setDraw(check);
-  
-};
+
+    if (check && !isWon) setDraw(check);
+    if (check) setModalTitle("Match Draw!!!");
+
+  };
 
   const checkWinner = () => {
+    let isWon = false;
     winningCombo.map((lineCoords) => {
       const [square1, square2, square3] = lineCoords;
       if (
@@ -57,22 +63,42 @@ export default function Home() {
         boardData[square1] === boardData[square3]
       ) {
         setWon(true);
+        setWinningLine(lineCoords)
+        setModalTitle(`Player ${!xTurn ? "X" : "O"} Won!!!`);
+        isWon = true;
       }
     });
+    return isWon;
+
   }
+
+  const reset = () => {
+    setBoardData({
+      0: "", 1: "", 2: "", 3: "", 4: "", 5: "", 6: "", 7: "", 8: "",
+    });
+    setXTurn(true);
+    setWon(false);
+    setWinningLine([]);
+    setDraw(false);
+    setModalTitle("");
+  };
 
   return (
     <div>
+      <div className={`modal ${modalTitle ? "show" : ""}`}>
+        <div className="modal-title">{modalTitle}</div>
+        <button onClick={reset}>New Game</button>
+      </div>
       <h1>Tic Tac Toe</h1>
       <div className="game">
         <div className="game-menu">
-          <p>{xTurn === true? "X Turn" : "O Turn"}</p>
+          <p>{xTurn === true ? "X Turn" : "O Turn"}</p>
           <p>{`Game Won:${won}| Game Draw: ${draw}`}</p>
         </div>
         <div className='game-board'>
           {[...Array(9)].map((v, idx) => {
             return (
-              <div key='idx' className='square' onClick={() => {updateBoardData(idx)}}>
+              <div key={idx} className={winningLine.includes(idx) ? 'square winner' : 'square'} onClick={() => { updateBoardData(idx) }}>
                 {boardData[idx]}
               </div>
             );
